d6592baa713482a57fbab8b9c59c7d632acf83df
114036-Benza-Tomas <114036@tecnicatura.frc.utn.edu.ar>
2024-03-18 18:51:06 -0300

    Startup configs
    Initial configs to begin the development

---
+++ gluten-away/src/main/java/com/glutenaway/GlutenAwayApplication.java
@@ -0,0 +1,13 @@
+package com.glutenaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlutenAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlutenAwayApplication.class, args);
+	}
+
+}
---
+++ gluten-away/src/test/java/com/glutenaway/GlutenAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glutenaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlutenAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
