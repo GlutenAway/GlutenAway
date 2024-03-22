76e11dadff4a5b1dc66ef991b5f21a06dfd0c089
114036-Benza-Tomas <114036@tecnicatura.frc.utn.edu.ar>
2024-03-21 23:24:53 -0300

    add main admin page

--- gluten-away-web/ga-web/src/app/app-routing.module.ts
+++ gluten-away-web/ga-web/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminProductsComponent } from './admin/admin-products/admin-products/admin-products.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminHomeComponent
+  },
+  {
+    path:'admin-products',
+    component: AdminProductsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gluten-away-web/ga-web/src/app/app.module.ts
+++ gluten-away-web/ga-web/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
-
+import { AdminProductsComponent } from './admin/admin-products/admin-products/admin-products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    AdminHomeComponent
+    AdminHomeComponent,
+    AdminProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
